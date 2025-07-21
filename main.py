a2d6fea4f5120d107bf913b2ffdefdfbc0db5eda
DESKTOP-ND1TSBH\leeba <leebada12@naver.com>
2025-07-21 23:42:26 +0900

    나도코딩 공부 시작~

--- main.py
+++ main.py
@@ -1,1 +1,56 @@
-print("st111111art")
+#20250721 나도코딩 시작
+print("hellow world")
+
+#자료형
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+print(5+8)
+print(2*8)
+print(3*(3+1))
+
+#문자형
+print('풍선')
+print("나비")
+print("ㅋㅋㅋㅋㅋ")
+print("ㅋ"*8)
+
+#boolean
+print(5 > 10)
+print(5 < 10)
+print(True)
+print(not True)
+print(not(5 > 10)) #연산이 어이없넹
+
+#변수
+#애완동물을 소개해 주세요~
+print("우리집 강아지 연탄이")
+print("연탄이는 4살, 산책 아주 좋아함")
+print("연탄이는 어른일까? True")
+
+#자료형 없이 변수 선언 어색함,,
+animal = "강아지"
+name = "연탄이"
+age = 4
+hobby = "산책"
+is_adult = age >= 3
+print("우리집 " + animal + name)
+print(name+"는" + str(age)+"살, "+hobby+" 아주 좋아함")
+print(name+"는 어른일까? "+str(is_adult)) #형변환
+print(name,"는 어른일까? ", is_adult) #형변환 ,로 하면 필요없넹 웃겨
+
+'''오 작은따옴표 세개 주석 잊고 있었어~~~~~'''
+
+# 퀴즈
+station = "사당"
+print(station + "행 열차가 들어오고 있습니다.")
+station = "신도림"
+print(station + "행 열차가 들어오고 있습니다.")
+station = "인천공항"
+print(station + "행 열차가 들어오고 있습니다.")
+
+#연산자
+# 2**3 ** 제곱
+# 10%3 % 나머지 구하기
+# // 몫
